ada251da2054b7617b54f56032250b9501087b9e
fanquake <fanquake@gmail.com>
2021-02-09 14:18:48 +0800

    Merge #21116: build: Disable --disable-fuzz-binary for gitian/guix builds
    cb151b797aeabb376d37ef058aee34c06d36d487 build: Disable --disable-fuzz-binary for guix builds (Hennadii Stepanov)
    fd7caae35fd3f83175247a79f7573e374779a851 build: Disable --disable-fuzz-binary for gitian builds (Hennadii Stepanov)
    
    Pull request description:
    
      Fuzz binary is not shipped to users.
      This PR saves hundreds MB of the disk space for containers.
    
    ACKs for top commit:
      MarcoFalke:
        review ACK cb151b797aeabb376d37ef058aee34c06d36d487
      fanquake:
        ACK cb151b797aeabb376d37ef058aee34c06d36d487
    
    Tree-SHA512: 858e3816576c307b47915bb05de79a28029beaef8835c01f1bd6a764a0cf7f7f63ef8c2dc2c5944cb36cc9f4788d9b0590b8a5dda96940167252ba371cdbd078

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -206,7 +206,7 @@ DISTNAME="$(basename "$SOURCEDIST" '.tar.gz')"
 ###########################
 
 # CONFIGFLAGS
-CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-gui-tests"
+CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-gui-tests --disable-fuzz-binary"
 case "$HOST" in
     *linux*) CONFIGFLAGS+=" --enable-glibc-back-compat" ;;
 esac
